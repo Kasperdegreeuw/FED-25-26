e5b0b63fc066d12ce841bb9d860fabb519541321
Kasperdegreeuw <kasper.de.greeuw@hva.nl>
2025-11-20 22:18:45 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+const menuButton = document.querySelector('header button')
+const navigatie = document.querySelector('header nav')
+
+menuButton.onclick = toggleMenu;
+
+function toggleMenu () {
+    navigatie.classList.toggle('menuOpen');
+}
